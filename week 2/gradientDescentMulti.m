7a24f96d1ab2ea79df4372b1f3f3932f50c867ec
Zayd Khan <43301355+zkhan04@users.noreply.github.com>
2023-03-05 19:50:01 -0600

    Update gradientDescentMulti.m

--- week 2/gradientDescentMulti.m
+++ week 2/gradientDescentMulti.m
@@ -1,5 +1,5 @@
 function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   GRADIENTDESCENTMULTI Performs gradient descent to learn theta
 %   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
 %   taking num_iters gradient steps with learning rate alpha
 
